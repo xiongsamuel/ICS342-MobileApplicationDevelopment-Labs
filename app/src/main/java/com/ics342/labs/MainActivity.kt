79c3d67ddfa26188fb634a2283c260195dbc09e0
Benjamin <benjamin.cassidy@metrostate.edu>
2023-07-17 16:24:05 -0500

    Lab 7

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -1,25 +1,54 @@
 package com.ics342.labs
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.ics342.labs.ui.theme.LabsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var hasPermission // state for tracking if the permission has been granted
+            var showPermissionRationale // state for tracking if the rationale should be shown
+            val context = LocalContext.current
+
+            val launcher = // The ManagedActivityResultLauncher for handling requesting permission
+
             LabsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+                        // if permission has been granted, show the LocationView
+                        // else if permission has not been granted, show a button to to request permission
+
+                        // --------
+
+                        // if user has denied permission and we should show the rationale, show the dialog
+                    }
                 }
             }
         }
@@ -27,17 +56,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+private fun LocationView() {
+    Text("Has Location Permission")
+}
+
+@Composable
+private fun PermissionRationaleDialog(
+    onConfirm: () -> Unit,
+    onCancel: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = {},
+        confirmButton = @Composable {
+            Button(onClick = { onConfirm() }) {
+                Text("OK")
+            }
+        },
+        dismissButton = @Composable {
+            Button(onClick = { onCancel() }) {
+                Text("No")
+            }
+        },
+        title = @Composable {
+            Text("Permissions Required")
+        },
+        text = @Composable {
+            Text("This app requires this permission to use this feature.")
+        }
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LabsTheme {
-        Greeting("Android")
-    }
+private fun checkOrRequestPermission(
+    context: Context,
+    permission: String,
+    launcher: ManagedActivityResultLauncher<String, Boolean>,
+    permissionGranted: () -> Unit
+) {
+    // Ask Android if the app has the permission with ContextCompat.checkSelfPermission
+
+    // if permission is granted, call the permission granted function
+    // if permission is not granted, launch the launcher for the permission
 }
