c8dab0aaabae70d2276242772661eeb510f407b6
Samuel Xiong <mf7423sf@go.minnstate.edu>
2023-07-17 17:07:54 -0500

    Lab 6

--- app/src/main/java/com/ics342/labs/Data.kt
+++ app/src/main/java/com/ics342/labs/Data.kt
@@ -1,3 +1,10 @@
 package com.ics342.labs
 
-// Add the data classes here.
+import com.squareup.moshi.Json
+
+data class Data(
+    @Json(name = "id") val id: Int,
+    @Json(name = "give_name") val giveName: String,
+    @Json(name = "family_name") val familyName: String,
+    @Json(name = "age") val age: Int,
+)
--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -4,13 +4,18 @@ import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ics342.labs.ui.theme.LabsTheme
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
@@ -26,9 +31,17 @@ class MainActivity : ComponentActivity() {
             LabsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    /*
-                    Display the items from the Json file in a LazyColumn
-                     */
+                    LazyColumn {
+                        items(10) { item ->
+                            Column {
+                                Text(text = data[item].id.toString())
+                                Text(text = data[item].giveName)
+                                Text(text = data[item].familyName)
+                                Text(text = data[item].age.toString())
+                                Spacer(modifier = Modifier.height(20.dp))
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -43,8 +56,8 @@ private fun loadData(resources: Resources): String {
 }
 
 @OptIn(ExperimentalStdlibApi::class)
-private fun dataFromJsonString(json: String): List</* put your data class name here */> {
+private fun dataFromJsonString(json: String): List<Data> {
     val moshi: Moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
-    val jsonAdapter: JsonAdapter<List</* put your data class name here */>> = moshi.adapter()
+    val jsonAdapter: JsonAdapter<List<Data>> = moshi.adapter()
     return jsonAdapter.fromJson(json) ?: listOf()
 }
